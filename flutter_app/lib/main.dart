2ee1e2f03f90240cbd7aad17f4a55c45c213c3d0
AhmetRidvan <ahmetridvanordulu@icloud.com>
2025-07-11 17:28:49 +0300

    fe
    ef

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,64 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+          seedColor: Colors.yellow,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    int count1 = 34;
-    double count2 = 2.3;
-    List myList = [];
-    Map<String, dynamic> har = {'value': 'key', 'Kara osman': 331.35};
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Image.asset('assets/images/as.jpg', height: double.infinity),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int value) {},
+          selectedIndex: 0,
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.add), label: 'Hi'),
+            NavigationDestination(icon: Icon(Icons.delete), label: 'No one'),
+          ],
+        ),
+        appBar: AppBar(backgroundColor: Colors.teal, title: Text('Hi')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('asd')],
+          ),
+        ),
       ),
     );
   }
