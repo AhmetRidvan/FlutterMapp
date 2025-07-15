2133d2323e36cedde47735e7b01192b3738ec853
AhmetRidvan <ahmetridvanordulu@icloud.com>
2025-07-15 22:01:20 +0300

    r
    rg

--- Flutter Zero To Hero/flutter_app/lib/main.dart
+++ Flutter Zero To Hero/flutter_app/lib/main.dart
@@ -12,31 +12,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+        appBar: AppBar(title: Text('Hi, im Ahmet')),
+      ),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.yellow,
+          seedColor: Colors.red,
           brightness: Brightness.dark,
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Container(decoration: BoxDecoration())),
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected: (int value) {},
-          selectedIndex: 0,
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.add), label: 'Hi'),
-            NavigationDestination(icon: Icon(Icons.add), label: 'Hi'),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('asd')],
-          ),
         ),
       ),
     );
